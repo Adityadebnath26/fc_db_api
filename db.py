0ec863d6c6c1d342bb6c2084727c0cf5d2773572
Adityadebnath26 <debnathaditya26@gmail.com>
2025-08-22 14:23:14 +0530

    db.py

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import pymysql
+import os
 
 def get_connection():
-    return pymysql.connect(host="localhost" ,user="root", password="Passwordmonenei", database="fc_database", port=3306, cursorclass=pymysql.cursors.DictCursor)
+    return pymysql.connect(host=os.getenv("DB_HOST") ,user=os.getenv("DB_USER"), password=os.getenv("DB_PASSWORD"), database=os.getenv("DB_NAME"), port=int(os.getenv("DB_PORT"), cursorclass=pymysql.cursors.DictCursor)
